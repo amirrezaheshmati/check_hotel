c47a3bd3a72e706f065cdf0e95fdb446e4b1a3cc
amirreza <hshmtyamyrrda631@gmail.com>
2025-07-24 20:03:48 +0330

    add Guest entry

--- checking/admin.py
+++ checking/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import GuestEntry
 # Register your models here.
+admin.site.register(GuestEntry)
--- checking/models.py
+++ checking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#guest , inspecter , commodity
+
+class GuestEntry(models.Model) :
+    name = models.CharField(max_length=60)
+    date_added = models.CharField(max_length=15)
+    hour_added = models.IntegerField()
+    number_phone = models.CharField(max_length=11)
+    pearson_code = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.name
