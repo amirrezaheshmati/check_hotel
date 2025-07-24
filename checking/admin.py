44cb2f251eb02e49e74d564b35eed63e297177ce
amirreza <hshmtyamyrrda631@gmail.com>
2025-07-24 20:15:38 +0330

    add commodity

--- checking/admin.py
+++ checking/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import GuestEntry
+from .models import GuestEntry , Commodity
 # Register your models here.
 admin.site.register(GuestEntry)
+admin.site.register(Commodity)
--- checking/models.py
+++ checking/models.py
@@ -9,6 +9,16 @@ class GuestEntry(models.Model) :
     hour_added = models.IntegerField()
     number_phone = models.CharField(max_length=11)
     pearson_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Commodity(models.Model) :
+    name = models.CharField(max_length=60)
+    date_added = models.CharField(max_length=15)
+    hour_added = models.IntegerField()
+    code = models.CharField(max_length=10)
+    descript = models.TextField()
     
     def __str__(self):
         return self.name
